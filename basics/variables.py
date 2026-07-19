c85ece2317d532ad40b9f897c38a2522d9da13e7
hoda-zarbi <h.zarbi81@gmail.com>
2026-07-19 14:43:02 -0700

    Add variables.py with Naruto example

---
+++ basics/variables.py
@@ -0,0 +1,30 @@
+"""
+Topic: Variables
+Example: Character Profile of Naruto Uzumaki (Shippuden)
+
+This file demonstrates how variables store different types of data.
+"""
+
+# String variables
+name = "Naruto Uzumaki"
+village = "Konohagakure"
+dream = "Become Hokage"
+birth_date = "October 10"
+
+# Integer variable
+age_shippuden = 17
+
+# Float variable
+height_shippuden = 166.0
+
+# Boolean variable
+is_hokage = False
+
+# Display character information
+print("Name:", name)
+print("Village:", village)
+print("Dream:", dream)
+print("Birth Date:", birth_date)
+print("Age (Shippuden):", age_shippuden)
+print("Height (Shippuden):", height_shippuden, "cm")
+print("Is Hokage:", is_hokage)
