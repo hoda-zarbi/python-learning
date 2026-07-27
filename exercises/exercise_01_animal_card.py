d9643bc5a70d851bf9a350f60d2741d8f8c5dd01
hoda-zarbi <h.zarbi81@gmail.com>
2026-07-27 08:41:22 -0700

    Add Exercise 01: Animal Card

---
+++ exercises/exercise_01_animal_card.py
@@ -0,0 +1,31 @@
+"""
+Exercise 01
+Topic: Variables
+
+Project:
+Animal Card
+
+Objective:
+Practice creating and using variables by building
+a simple animal profile.
+"""
+
+# Animal information
+name = "Red Panda"
+species = "Mammal"
+habitat = "Eastern Himalayas"
+diet = "Bamboo, Fruits, Eggs"
+weight = 6.2
+life_span = 14
+is_endangered = True
+
+# Display the animal card
+print("========== Animal Card ==========")
+print("Name:", name)
+print("Species:", species)
+print("Habitat:", habitat)
+print("Diet:", diet)
+print("Weight:", weight, "kg")
+print("Life Span:", life_span, "years")
+print("Is Endangered:", is_endangered)
+print("=================================")
