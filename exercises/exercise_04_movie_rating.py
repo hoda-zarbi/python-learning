030d8cdd8be1d9af2cc92754ab557ff5bdbbe346
Hoda Zarbi <h.zarbi81@gmail.com>
2026-07-31 08:34:24 -0700

    Add Exercise 04: Movie Rating

---
+++ exercises/exercise_04_movie_rating.py
@@ -0,0 +1,40 @@
+"""
+Exercise 04
+
+Topic:
+- Variables
+- Data Types
+- Input
+- Output
+- Conditional Statement
+
+Project:
+movie rating
+
+Objective:
+Practice using variables, data types, user input,
+conditional statements, and output by creating
+a simple movie rating program.
+"""
+print()
+
+movie_title = input("Enter Movie Title: ")
+imdb_rating = float(input("Enter IMDB: "))
+number_of_votes = int(input("Enter Number of Votes: "))
+
+if imdb_rating >= 7.5 and number_of_votes >= 50:
+    recommendation = "Highly Recommended!" 
+    
+elif imdb_rating >= 5.5 and number_of_votes >= 50:
+    recommendation = "Worth Watching:)"
+    
+else:
+    recommendation = "Not Recommended"
+    
+print("========================")       
+print("====== Movie Rating ======")
+print("Movie Title: ", movie_title)
+print("IMDB Rating: ", imdb_rating)
+print("Number of Votes: ", number_of_votes)
+print("Recommendation: ", recommendation)
+print("========================")
