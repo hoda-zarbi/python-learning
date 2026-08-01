87312ee0c1d9683c5aa6b98f80afa6725abd760f
hoda-zarbi <h.zarbi81@gmail.com>
2026-08-01 19:50:39 +0330

    Update exercise_04_movie_rating.py

--- exercises/exercise_04_movie_rating.py
+++ exercises/exercise_04_movie_rating.py
@@ -17,7 +17,7 @@ conditional statements, and output by creating
 a simple movie rating program.
 """
 print()
-
+# Get movie information from the user
 movie_title = input("Enter Movie Title: ")
 imdb_rating = float(input("Enter IMDB: "))
 number_of_votes = int(input("Enter Number of Votes: "))
@@ -30,7 +30,7 @@ elif imdb_rating >= 5.5 and number_of_votes >= 50:
     
 else:
     recommendation = "Not Recommended"
-    
+# Display the movie rating report
 print("========================")       
 print("====== Movie Rating ======")
 print("Movie Title: ", movie_title)
